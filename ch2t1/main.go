d8ddd76a6f29202f8fb82acbde0bb56f1beec4c6
Slava <qdrow91@mail.ru>
2021-07-19 18:13:08 +0300

    ch2tsk1

---
+++ ch2t1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/qdrow-tch/errors_workout/logging"
+)
+
+func main() {
+	var value int = 100
+	mass := []int{5, 15, 0, 8, 10}
+
+	defer func() {
+		if v := recover(); v != nil {
+			logging.Push_to_logfile("Got panic plz chech your code")
+
+		}
+	}()
+
+	for i, vl := range mass {
+		fmt.Printf("%d: %d / %d = %f \n", i+1, value, vl, value/vl)
+	}
+}
