f9be7144acb1ed6effb00eb8509b7b9cd4ab5956
qdrow <qdrow91@mail.ru>
2021-08-02 02:04:42 +0300

    Done counter

--- gorcounter/main.go
+++ gorcounter/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 
+	counter := make(chan int, 1)
+	counter <- 1
+
+	for i := 0; i < 5; i++ {
+		go func(id int) {
+			for {
+				cach := <-counter
+				fmt.Println("Счетчик: %d", cach)
+				if cach == 1000 {
+					break
+				}
+				cach++
+				counter <- cach
+			}
+		}(i)
+	}
+
+	time.Sleep(6 * time.Second)
 }
