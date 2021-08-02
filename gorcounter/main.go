f39a33d48a4e0bb064d0c3c6967a4ad0a4ed85e5
Slava <qdrow91@mail.ru>
2021-08-02 14:00:15 +0300

    Auto stash before merge of "counter" and "origin/counter"

--- gorcounter/main.go
+++ gorcounter/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -10,19 +14,36 @@ func main() {
 	counter := make(chan int, 1)
 	counter <- 1
 
-	for i := 0; i < 5; i++ {
-		go func(id int) {
+	ctx := context.Background()
+	cancelableCtx, cancelfunc := context.WithTimeout(ctx, 1*time.Second)
+
+	sigchecker := make(chan os.Signal, 2)
+	signal.Notify(sigchecker, syscall.SIGTERM)
+
+	go func() {
+		gotsign := <-sigchecker
+		fmt.Println(gotsign)
+		cancelfunc()
+	}()
+
+	for i := 0; i < 10; i++ {
+		go func(id int, ctx context.Context) {
 			for {
-				cach := <-counter
-				if cach == 1000 {
-					break
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					cach := <-counter
+					if cach == 1000 {
+						break
+					}
+					cach++
+					counter <- cach
+					fmt.Println("Worker ", id, " push value: ", cach)
 				}
-				cach++
-				counter <- cach
-				fmt.Println("Worker ", id, " push value: ", cach)
 			}
-		}(i)
+		}(i, cancelableCtx)
 	}
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(7 * time.Second)
 }
