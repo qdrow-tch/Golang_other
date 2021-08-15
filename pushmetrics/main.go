2a88188854686adea39b3200927b190d0407f512
Slava <qdrow91@mail.ru>
2021-08-15 19:26:40 +0300

    crt exporter to Prometheus

--- pushmetrics/main.go
+++ pushmetrics/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/qdrow-tch/Golang/tree/pushmetrics/pushmetrics/fileconverter"
+	"github.com/qdrow-tch/Golang/tree/pushmetrics/pushmetrics/metricscreater"
+)
+
 func main() {
+
+	exrate, err := fileconverter.ConvertToER("./exchangerate.yaml")
+	if err != nil {
+		log.Fatalln("невозможно конвертировать в ExchangeRate: ", err)
+	}
+
+	http.HandleFunc("/metrics", metricscreater.GetERmetrics(*exrate))
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln("Не удалось запустить сервер", err)
+	}
 
 }
