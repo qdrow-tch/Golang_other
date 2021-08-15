21988a9e239bb1b8eacc3620d41ca67d1aa7a93a
Slava <qdrow91@mail.ru>
2021-08-15 19:52:04 +0300

    crt config

--- pushmetrics/main.go
+++ pushmetrics/main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
+	"yaml-2"
 
 	"github.com/qdrow-tch/Golang/tree/pushmetrics/pushmetrics/fileconverter"
 	"github.com/qdrow-tch/Golang/tree/pushmetrics/pushmetrics/metricscreater"
 )
 
+type Config struct {
+	FileConvert string
+	Port        string
+}
+
 func main() {
 
-	exrate, err := fileconverter.ConvertToER("./exchangerate.yaml")
+	var config Config
+	yaml_config, err := ioutil.ReadFile("./config.yaml")
+	if err != nil {
+		log.Fatalln("Невозможно открыть файл конфигурации")
+	}
+	err = yaml.Unmarshal(yaml_config, &config)
+	if err != nil {
+		log.Fatalln("Невозможно преобразовать из .yaml в Config")
+	}
+
+	exrate, err := fileconverter.ConvertToER(config.FileConvert)
 	if err != nil {
 		log.Fatalln("невозможно конвертировать в ExchangeRate: ", err)
 	}
 
 	http.HandleFunc("/metrics", metricscreater.GetERmetrics(*exrate))
-	err = http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(config.Port, nil)
 	if err != nil {
 		log.Fatalln("Не удалось запустить сервер", err)
 	}
