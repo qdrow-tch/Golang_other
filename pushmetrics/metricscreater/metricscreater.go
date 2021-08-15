8357fd2d63f820852a83e98018263ce9178799ba
Slava <qdrow91@mail.ru>
2021-08-15 19:23:09 +0300

    crt pack metrics

--- pushmetrics/metricscreater/metricscreater.go
+++ pushmetrics/metricscreater/metricscreater.go
@@ -1,1 +1,18 @@
 package metricscreater
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/qdrow-tch/Golang/tree/pushmetrics/pushmetrics/fileconverter"
+)
+
+func GetERmetrics(er fileconverter.ExchangeRates) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var responseText string
+		for i := range er.Currencies {
+			responseText += fmt.Sprintf("exchange_rate{name=%s, value=%F}\n", er.Currencies[i].Name, er.Currencies[i].Value)
+		}
+		fmt.Fprintf(w, responseText)
+	}
+}
